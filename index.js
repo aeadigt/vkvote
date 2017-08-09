844c08d4e74e0c0fe651459fa598d62617e22b03
aea <aea@digt.ru>
2017-08-09 15:34:26 +0300

    Добавлено создание голосования и публикация поста с видео и голосованием

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+console.log('module index started!');
+
 let Vk = require('vksdk');
 
 let vk = new Vk({
@@ -18,11 +20,12 @@ vk.on('serverTokenReady', (data) => {
 // Turn on requests with access tokens
 vk.setSecureRequests(true);
 
+// https://oauth.vk.com/authorize?client_id=6141200&redirect_uri=https://oauth.vk.com/blank.html&response_type=token&scope=notify,friends,photos,audio,video,docs,notes,pages,status,wall,groups,messages,email,notifications,stats,ads,market,offline
 
 // ************************** Запросы **************************
-
+var access_token = '750188ab8387a4cf2737462be932866c072e1335c2d52b41dee26f0d5ebd1763e9e981962c18a99be4bb9';
 // First you have to pass access_token from client side JS code
-vk.setToken('6e4e86856e4e86856e4e86856e6e13339566e4e6e4e868537c7e09391da28f8edcca67f');
+vk.setToken(access_token);
 
 // Получаем список постов с видео
 vk.request('wall.get', { 
@@ -35,23 +38,62 @@ vk.request('wall.get', {
 
         let videoPosts = [];
 
-        if (data && data.response && data.response.items &&
-            Array.isArray(data.response.items) ) {
+        if (data && data.response && data.response.items
+             && Array.isArray(data.response.items) ) {
 
             data.response.items.forEach((item) => {
-                if (item && item.attachments && item.attachments[0] && 
-                    item.attachments[0].type && item.attachments[0].type == 'video') {
+                if (item && item.attachments && item.attachments[0]
+                    && item.attachments[0].type && item.attachments[0].type == 'video') {
                     videoPosts.push(item)
                 }
             });
         }
 
-        console.log('\r\n count video Posts: ', videoPosts.length);
-
-        videoPosts.forEach((item) => {
-            console.log('\r\n videoPost: ', item);
-        });
+        sendAllNewVideo(videoPosts);
     }
 );
 
-console.log('module index started!');
+function sendAllNewVideo(videoPosts) {
+    videoPosts.forEach((item) => {
+        if (item && item.attachments && item.attachments[0] 
+            && item.attachments[0].video.owner_id && item.attachments[0].video.id) {
+
+            let video = 'video' + item.attachments[0].video.owner_id + '_' + item.attachments[0].video.id;
+            createPoll(video);
+        }
+    });
+};
+
+function createPoll(video) {
+    vk.request('polls.create', { 
+            owner_id: '-150899833',
+            add_answers: '["5", "4", "3", "2", "1"]',
+            question: 'Ваша оценка за видео',
+            access_token: access_token
+        }, (data) => {
+            console.log('\r\n polls.create: ', data);
+            console.log('\r\n polls.create data.response.id: ', data.response.id);
+
+            if (data && data.response && data.response.id && data.response.owner_id) {
+                let poll = 'poll' + data.response.owner_id + '_' + data.response.id;
+                sendNewVideo(video, poll);
+            }
+    });
+    
+}
+
+function sendNewVideo(video, poll) {
+    // Отправляем видео
+    vk.request('wall.post', { 
+            // owner_id: '-150899833',
+            owner_id: '-150899833',
+            message: 'Новое видео',
+            attachments: video + ',' + poll,
+            access_token: access_token
+        }, (data) => {
+            console.log('\r\n wall.post: ', data);
+        }
+    );
+}
+
+console.log('module index ended!');
